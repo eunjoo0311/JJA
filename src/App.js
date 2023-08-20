e20d501787480a8e41533fdeac609c1e3fe2a7b2
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-08-21 02:48:06 +0900

    🎨 style: globalStyle, theme 추가 (#3)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { appRouter } from './pages/Router';
 import { RouterProvider } from 'react-router-dom';
+import { Global, ThemeProvider } from '@emotion/react';
+import globalStyle from './style/GlobalStyle';
+import theme from './style/theme';
 
 export default function App() {
     return (
         <>
-            <RouterProvider router={appRouter} fallbackElement={null} />
+            <ThemeProvider theme={theme}>
+                <Global styles={globalStyle} />
+                <RouterProvider router={appRouter} fallbackElement={null} />
+            </ThemeProvider>
         </>
     );
 }
