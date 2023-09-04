cc68e8a0f8b572db746d81d59f096ddb5d08c104
eunjoo0311 <yyy7423@naver.com>
2023-09-05 03:24:01 +0900

    ✨ feat: header 기능 구현
    header 기능 구현 및 header 파일 위치 변경했습니다

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
 import React from 'react';
-import github from '../assets/images/github.svg';
-import logo from '../assets/images/JJA.svg';
+import github from '../../assets/images/github.svg';
+import logo from '../../assets/images/JJA.svg';
 import styled from '@emotion/styled';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Wrapper = styled.nav`
     display: flex;
@@ -30,12 +31,34 @@ const ImgBtn = styled.button`
 `;
 
 export default function Header() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    function goHome() {
+        navigate('/');
+    }
+
+    function goInfo() {
+        if (location.pathname === '/info') {
+            navigate('/');
+        } else {
+            navigate('/info');
+        }
+    }
+
+    function goGithub() {
+        const githubURL = 'https://github.com/Jobs-Js';
+        window.open(githubURL, '_blank');
+    }
+
     return (
         <Wrapper>
-            <Logo src={logo} alt="Logo" />
+            <Logo src={logo} alt="Logo" onClick={goHome} />
             <div>
-                <TextBtn>INFO</TextBtn>
-                <ImgBtn>
+                <TextBtn onClick={goInfo}>
+                    {location.pathname === '/info' ? 'CLOSE' : 'INFO'}
+                </TextBtn>
+                <ImgBtn onClick={goGithub}>
                     <img src={github} alt="GitHub" />
                 </ImgBtn>
             </div>
