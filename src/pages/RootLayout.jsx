41a30036cdfcf2cb59025578eebea7155941ca75
eunjoo0311 <yyy7423@naver.com>
2023-09-05 03:26:09 +0900

    Revert "✨ feat: header 기능 구현(#4)"
    This reverts commit cc68e8a0f8b572db746d81d59f096ddb5d08c104.

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '../components/Header/Header';
+import Header from '../components/header';
 
 export default function RootLayout() {
     return (
