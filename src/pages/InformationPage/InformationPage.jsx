b1d7c597b294880c1a01f7f1a706f392a3f1a57f
eunjoo0311 <yyy7423@naver.com>
2023-09-26 22:05:59 +0900

    🎨 style: info 페이지 마크업 (#22)

--- src/pages/InformationPage/InformationPage.jsx
+++ src/pages/InformationPage/InformationPage.jsx
@@ -1,5 +1,54 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
 import React from 'react';
+import styled from '@emotion/styled';
+import bookShelfImg from '../../assets/images/bookShelf.png';
 
+const InfoWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    height: calc(100vh - 104px);
+    gap: 40px;
+`;
+
+const BookShelf = styled.img`
+    position: absolute;
+    bottom: 0;
+`;
+
+const Info = styled.p`
+    width: 100%;
+    color: ${({ theme }) => theme.colors.white};
+    font-size: ${({ theme }) => theme.fontSize.medium};
+    padding: 0 120px;
+
+    &:nth-of-type(1) {
+        padding-top: 100px;
+    }
+`;
 export default function InformationPage() {
-    return <div>InformationPage</div>;
+    return (
+        <InfoWrapper>
+            <BookShelf src={bookShelfImg} />
+            <Info>
+                JJA(JsJobs Archive)는 JJA(JsJobsArchive)는 'Js-Jobs'라는 스터디
+                그룹의 활동을 아카이빙하여 지속적인 학습과 지식 공유를 촉진하는
+                목적으로 만들어진 책장 속 책 형식의 웹사이트입니다.
+            </Info>
+            <Info>
+                Js-Jobs 스터디 그룹은 코어 자바스크립트 튜토리얼을 기반으로 매주
+                정해진 분량을 읽고 학습하며, 그 주제에 따라 스터디원들이 발표를
+                진행합니다. 발표 이후에는 자유로운 토론과 질의응답을 통해 서로의
+                의견을 나누고 궁금한 점을 해결하는 시간을 가졌습니다.
+            </Info>
+
+            <Info>
+                JJA 프로젝트는 이러한 활동을 기록하고 보존함으로써, 스터디
+                그룹의 누적된 지식과 경험을 정리하여 온라인 플랫폼에 제공합니다.
+                각 주제별로 발표 내용을 정리하여 책장을 구경하듯이 찾아볼 수
+                있습니다.
+            </Info>
+        </InfoWrapper>
+    );
 }
