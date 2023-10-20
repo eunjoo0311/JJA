04c36a9d3e8396dcce322c636de774d9a08771da
NAYEONG OH <102240503+ony540@users.noreply.github.com>
2023-10-20 22:13:59 +0900

    ✨ feat: 모바일 chapter 페이지 기능구현 (#33)

--- src/pages/BookcontentsPage/ChapterPage/ChapterPage.jsx
+++ src/pages/BookcontentsPage/ChapterPage/ChapterPage.jsx
@@ -1,4 +1,10 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, {
+    useEffect,
+    useLayoutEffect,
+    useMemo,
+    useRef,
+    useState,
+} from 'react';
 import { useNavigate, useLocation, useParams } from 'react-router-dom';
 import { Rnd } from 'react-rnd';
 import {
@@ -12,28 +18,40 @@ import {
     prevButtonCss,
     BookBox,
     PptImgBox,
+    ScriptBox,
 } from './ChapterPageStyle';
 import pptImg from '../../../db/pptImg.json';
 import content from '../../../db/content.json';
 import chapterList from '../../../db/chapter.json';
 import CircleButton from '../../../components/CircleButton/CircleButton';
 import Spinner from '../../../components/Spinner/Spinner';
+import debounce from '../../../utils/debounce';
 
 export default function ChapterPage() {
     const navigate = useNavigate();
     const { hash } = useLocation();
     const { bookname, chapter } = useParams();
     const realChapter = chapter.replaceAll('-', ' ');
-    const pageNumber = parseInt(hash.slice(1));
+    const [pageNumber, setPageNumber] = useState(parseInt(hash.slice(1)));
 
-    const selectedColor = chapterList.contentList.find(item =>
-        item.indexData.includes(realChapter),
-    )?.color;
-
+    const selectedColor = useMemo(
+        () =>
+            chapterList.contentList.find(item =>
+                item.indexData.includes(realChapter),
+            )?.color,
+        [realChapter],
+    );
     const pptImgString = new Map(Object.entries(pptImg)).get(realChapter);
     const [firstImageLoaded, setFirstImageLoaded] = useState(false);
     const [pptImgData, setPptImgData] = useState([]);
+    const contentData = new Map(Object.entries(content)).get(realChapter);
 
+    const [isMovetoIndex, setIsMovetoIndex] = useState(false);
+    const scriptBoxRef = useRef();
+    const paragraphRef = useRef([]);
+    const [isLooped, setIsLooped] = useState(false);
+
+    // 이미지 프리로드
     const pptImgPreload = () => {
         for (let i = 0; i < pptImgString.length; i++) {
             const pptImg = new Image();
@@ -51,8 +69,66 @@ export default function ChapterPage() {
         pptImgPreload();
     }, []);
 
-    const contentData = new Map(Object.entries(content)).get(realChapter);
-    const [isMovetoIndex, setIsMovetoIndex] = useState(false);
+    // 반응형 확인
+    const [isPC, setIsPC] = useState(window.innerWidth > 1024);
+
+    const handleResize = debounce(() => {
+        setIsPC(window.innerWidth > 1024);
+    }, 100);
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    // 해시, 스크롤 이동
+    useEffect(() => {
+        if (!isPC) {
+            navigate(`#${pageNumber}`);
+            if (isLooped) {
+                paragraphRef.current[pageNumber - 1].scrollIntoView({
+                    block: 'start',
+                });
+                setIsLooped(false);
+            } else {
+                paragraphRef.current[pageNumber - 1].scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                });
+            }
+        }
+    }, [pageNumber, isPC]);
+
+    //스크롤에 따른 PageNumber 지정
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            entries => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        const index = paragraphRef.current.indexOf(
+                            entry.target,
+                        );
+                        if (index !== -1) {
+                            setPageNumber(index + 1);
+                        }
+                    }
+                });
+            },
+            {
+                root: scriptBoxRef.current,
+                rootMargin: '0px 0px -95% 0px',
+            },
+        );
+
+        paragraphRef.current.forEach(el => observer.observe(el));
+        return () => {
+            if (paragraphRef.current instanceof Element) {
+                paragraphRef.current.forEach(el => observer.unobserve(el));
+            }
+        };
+    }, []);
 
     const onClickMenu = () => {
         setIsMovetoIndex(true);
@@ -68,16 +144,18 @@ export default function ChapterPage() {
 
     const onClickNextButton = () => {
         if (pageNumber < pptImgString.length) {
-            navigate(`#${pageNumber + 1}`);
+            setPageNumber(pageNumber + 1);
         } else {
-            navigate('#1');
+            setPageNumber(1);
+            setIsLooped(true);
         }
     };
     const onClickPrevButton = () => {
         if (pageNumber > 1) {
-            navigate(`#${pageNumber - 1}`);
+            setPageNumber(pageNumber - 1);
         } else {
-            navigate(`#${pptImgString.length}`);
+            setPageNumber(pptImgString.length);
+            setIsLooped(true);
         }
     };
 
@@ -95,7 +173,36 @@ export default function ChapterPage() {
                 </LeftPage>
                 <RightPage>
                     <Title>{realChapter}</Title>
-                    <Script>{contentData[pageNumber - 1]}</Script>
+                    {!isPC && (
+                        <PptImgBox>
+                            {firstImageLoaded ? (
+                                <img
+                                    src={pptImgData[pageNumber - 1].src}
+                                    draggable="false"
+                                    alt="ppt 장표"
+                                />
+                            ) : (
+                                <Spinner />
+                            )}
+                        </PptImgBox>
+                    )}
+
+                    <ScriptBox ref={scriptBoxRef}>
+                        {isPC ? (
+                            <Script>{contentData[pageNumber - 1]} </Script>
+                        ) : (
+                            contentData.map((paragraph, index) => (
+                                <Script
+                                    key={index}
+                                    ref={el =>
+                                        (paragraphRef.current[index] = el)
+                                    }
+                                >
+                                    {paragraph}
+                                </Script>
+                            ))
+                        )}
+                    </ScriptBox>
                     <PageNum>
                         {pageNumber}/{pptImgString.length}
                     </PageNum>
@@ -113,31 +220,33 @@ export default function ChapterPage() {
                         />
                     </nav>
                 </RightPage>
-                <Rnd
-                    default={{
-                        x: 300,
-                        y: 430,
-                        width: 620,
-                        height: 348,
-                    }}
-                    minWidth={500}
-                    minHeight={270}
-                    maxWidth={830}
-                    maxHeight={480}
-                    bounds="window"
-                >
-                    <PptImgBox>
-                        {firstImageLoaded ? (
-                            <img
-                                src={pptImgData[pageNumber - 1].src}
-                                draggable="false"
-                                alt="ppt 장표"
-                            />
-                        ) : (
-                            <Spinner />
-                        )}
-                    </PptImgBox>
-                </Rnd>
+                {isPC && (
+                    <Rnd
+                        default={{
+                            x: 300,
+                            y: 430,
+                            width: 620,
+                            height: 348,
+                        }}
+                        minWidth={500}
+                        minHeight={270}
+                        maxWidth={830}
+                        maxHeight={480}
+                        bounds="window"
+                    >
+                        <PptImgBox>
+                            {firstImageLoaded ? (
+                                <img
+                                    src={pptImgData[pageNumber - 1].src}
+                                    draggable="false"
+                                    alt="ppt 장표"
+                                />
+                            ) : (
+                                <Spinner />
+                            )}
+                        </PptImgBox>
+                    </Rnd>
+                )}
             </BookBox>
         </ChapterPageWrapper>
     );
